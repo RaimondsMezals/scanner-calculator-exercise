d27a185dfb8cae078c1231d07e38aba0a27e9473
RaimondsMezalsQA <RMezals@qa.com>
2021-01-14 11:00:02 +0000

    improved scanner

--- src/com/qa/main/Runner.java
+++ src/com/qa/main/Runner.java
@@ -6,44 +6,9 @@ public class Runner {
 
 	public static void main(String[] args) {
 		
-		Scanner input = new Scanner(System.in);
+		ScannerC sc = new ScannerC();
 		
-		System.out.println("Please write which function you would like to use.\n 1. addition 2. subtraction 3. multiplication 4. division");
-		
-		String result = input.nextLine();
-		
-		Calculator c = new Calculator();
-		
-		switch(result) {
-		case "addition":
-			System.out.println("Please enter an int for calculation");
-			result = input.nextLine();
-			System.out.println(c.addition(Integer.parseInt(result)));
-			break;
-		case "subtraction":
-			System.out.println("Please enter an int for calculation");
-			result = input.nextLine();
-			System.out.println(c.subtraction(Integer.parseInt(result)));
-			break;
-		case "multiplication":
-			System.out.println("Please enter an int for calculation");
-			result = input.nextLine();
-			System.out.println(c.multiplication(Integer.parseInt(result)));
-			break;
-		case "division":
-			System.out.println("Please enter an int for calculation");
-			result = input.nextLine();
-			c.division(Integer.parseInt(result));
-			break;
-		default:
-			System.out.println("Unexpected input");
-			break;
-		
-		}
-		
-		
-		input.close();
-		System.out.println("input has been closed!\n\t- Exit!");
+		sc.scanner();
 		
 	}
 
